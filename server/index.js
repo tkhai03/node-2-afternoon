017abff2b4a9df9d0371c8eb52ea25dd586e094b
Thang Khai <tnkhai94@gmail.com>
2020-08-25 15:51:23 -0600

    initial commit

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const app = express()
+
+app.use(express.json())
+
+const port = 3001
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`)
+})
